568aaae455262b40837c070eb439d4f2a8770c87
deshwal <deshwal@scaligent.com>
2013-05-02 16:06:00 -0700

    Add a tool to install the config as symlinks to prevent double-editing.

---
+++ bin/install_config.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+GITDIR="$PWD/.git"
+if [ ! -d "$GITDIR" ];
+then
+  echo "Execute command from the root of the config git repository"
+  exit 1
+fi
+
+for FILE in $(ls -a)
+do
+  if [ "$FILE" != "." -a "$FILE" != ".." -a "$FILE" != ".git" ];
+  then
+    SRC="$PWD/$FILE"
+    DST="/home/deshwal/$FILE"
+    rm -f "$DST"
+    ln -s "$SRC" "$DST"
+  fi
+done
